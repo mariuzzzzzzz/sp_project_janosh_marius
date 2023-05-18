87fe47c2b2496dde781019aad06d0476cac9b3b4
Marius Affolter <mariusaffolter@outlook.de>
2023-05-18 17:33:54 +0200

    alles plotte

--- python_sp_calc/calc.py
+++ python_sp_calc/calc.py
@@ -1,34 +1,40 @@
 import sqlite3
 import matplotlib.pyplot as plt
+import pandas as pd
 
 def graph(employee, covid):
     # Connect to the SQLite database
     conn = sqlite3.connect('/Users/mariusaffolter/Documents/GitHub/sp_project_janosh_marius/backend_sp_db/Database/project_SP.db')
-    cursor = conn.cursor()
 
-    # Execute a SELECT query to fetch data from a table
-    cursor.execute(f"SELECT * FROM Combined_Data")
-    rows1 = cursor.fetchall()
+    # Read data from tables using pandas
+    rows1 = pd.read_sql_query("SELECT * FROM Covid_Data", conn)
+    rows2 = pd.read_sql_query("SELECT * FROM SP_Project_Sums", conn)
 
-    cursor.execute(f"SELECT * FROM SP_Project_Sums")
-    rows2 = cursor.fetchall()
-    
-    # Store the values in a list
-    employee_list = []
-    covid_list = []
-    
     # Close the database connection
     conn.close()
 
-    # Extract x and y values from the data
-    x_values = [row[employee] for row in employee_list]  # Assuming x values are in the first column
-    y_values = [row[covid] for row in covid_list]  # Assuming y values are in the second column
+    # Store the values in separate lists
+    dates = rows1['date']  # Assuming the date column is named 'date' in the Covid_Data table
+    employee_list = rows2.iloc[:, employee]
+    covid_list = rows1.iloc[:, covid]
 
-    # Create a line graph using Matplotlib
-    plt.plot(x_values, y_values)
-    plt.xlabel('x')
-    plt.ylabel('y')
-    plt.title('COVIDxEMPLOYEES')
+    # Create a line graph using Matplotlib with dual y-axes
+    fig, ax1 = plt.subplots()
+
+    ax1.plot(dates, employee_list, 'bo-', label='Employees')
+    ax1.set_xlabel('Dates')
+    ax1.set_ylabel('Employees', color='b')
+
+    ax2 = ax1.twinx()  # Create a secondary y-axis
+    ax2.plot(dates, covid_list, 'ro-', label='COVID')
+    ax2.set_ylabel('COVID', color='r')
+
+    # Set title and legends
+    plt.title('COVID and Employees Over Time')
+    ax1.legend(loc='upper left')
+    ax2.legend(loc='upper right')
+
+    plt.xticks(rotation=45)  # Rotate x-axis labels for better readability
     plt.show()
 
-graph(2, 2)
+graph(6,1)
