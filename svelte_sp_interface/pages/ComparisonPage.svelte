e1d77bb40fa4e8f4bfe7d910bc6aecae55962dcd
Marius Affolter <mariusaffolter@outlook.de>
2023-05-18 11:24:23 +0200

    pages

---
+++ svelte_sp_interface/pages/ComparisonPage.svelte
@@ -0,0 +1,8 @@
+<script>
+    // ComparisonPage.svelte
+    export let activePage;
+  </script>
+  
+  <h1>Comparison Page</h1>
+  
+  <!-- Rest of the content specific to the comparison page -->  
--- svelte_sp_interface/src/App.svelte
+++ svelte_sp_interface/src/App.svelte
@@ -1,5 +1,54 @@
 <script>
-	let name = 'world';
+	// Track the active page
+	export let activePage = "";
+
+	// Handle navigation clicks
+	function navigate(page) {
+		activePage = page;
+	}
 </script>
 
-<h1>Hello {name}!</h1>
+<nav>
+	<ul>
+	  <li>
+		<a href="#" class:selected={activePage === 'data'} on:click={() => navigate('data')}>
+		  Data
+		</a>
+	  </li>
+	  <li>
+		<a href="#" class:selected={activePage === 'comparison'} on:click={() => navigate('comparison')}>
+		  Comparison
+		</a>
+	  </li>
+	</ul>
+  </nav>
+  
+<style>
+	nav {
+    background-color: #f1f1f1;
+    padding: 10px;
+  }
+
+  ul {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    display: flex;
+  }
+
+  li {
+    margin-right: 10px;
+  }
+
+  a {
+    text-decoration: none;
+    color: #333;
+    padding: 5px;
+    border-radius: 3px;
+  }
+
+  a.selected {
+    background-color: #333;
+    color: #fff;
+  }
+</style>
