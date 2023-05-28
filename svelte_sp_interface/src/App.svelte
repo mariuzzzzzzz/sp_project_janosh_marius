76e32787a38dd8da8b2bc721a4fc0f34205589f4
JanoshWerlen <werleja1@students.zhaw.ch>
2023-05-28 11:46:48 +0200

    commit

--- svelte_sp_interface/src/App.svelte
+++ svelte_sp_interface/src/App.svelte
@@ -7,23 +7,35 @@
 </script>
 
 <div id="app">
-	<nav class="navbar navbar-expand-lg bg-body-tertiary justify-content-center">
+	<nav
+		class="navbar navbar-expand-lg bg-body-tertiary justify-content-center"
+	>
 		<div class="container-fluid d-flex flex-column align-items-center">
-		  <a class="navbar-brand mb-3" href="/">COVIDxEMPLOYEES</a>
-		  <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
-			<ul class="navbar-nav">
-			  <li class="nav-item">
-				<button class="btn btn-sm btn-outline-primary" type="button" onclick="location.href='#/'">Home</button>
-			  </li>
-			  <li class="nav-item">
-				<button class="btn btn-sm btn-outline-primary" type="button" onclick="location.href='#/ComparisonPage'">Comparison</button>
-			  </li>
-			</ul>
-		  </div>
+			<a class="navbar-brand mb-3" href="/">COVIDxEMPLOYEES</a>
+			<div
+				class="collapse navbar-collapse justify-content-center"
+				id="navbarNav"
+			>
+				<ul class="navbar-nav">
+					<li class="nav-item">
+						<button
+							class="btn btn-sm btn-outline-primary"
+							type="button"
+							onclick="location.href='#/'">Home</button
+						>
+					</li>
+					<li class="nav-item">
+						<button
+							class="btn btn-sm btn-outline-primary"
+							type="button"
+							onclick="location.href='#/ComparisonPage'"
+							>Comparison</button
+						>
+					</li>
+				</ul>
+			</div>
 		</div>
-	  </nav>
-	  
-	  
+	</nav>
 
 	<Router {routes} />
 </div>
@@ -34,10 +46,10 @@
 		margin-left: 10px;
 	}
 	.navbar-brand {
-    display: inline-block;
-    background-color: black;
-    color: white;
-    padding: 5px 10px;
+		display: inline-block;
+		background-color: black;
+		color: white;
+		padding: 5px 10px;
 	}
 	:global(body) {
 		background-color: rgba(68, 149, 255, 0.239);
